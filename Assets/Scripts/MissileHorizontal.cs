2915c8d85245abc2d3fe357a7a930f0b53098ad6
Berke Soysal <Berke.Soysal@sahibinden.com>
2020-06-02 00:31:10 +0300

    files refactored

---
+++ Assets/Scripts/MissileHorizontal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public void OnMouseDown()
+    { 
+        BlockSpawner blockSpawner = GameObject.Find("spawner").GetComponent<BlockSpawner>();
+        blockSpawner.getMissiledBrick(gameObject.transform);
+    }
+}
---
+++ Assets/Scripts/MissileVertical.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class MissileUpside : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        BlockSpawner blockSpawner = GameObject.Find("spawner").GetComponent<BlockSpawner>();
+        blockSpawner.getMissiledBrickUpside(gameObject.transform);
+    }
+}
